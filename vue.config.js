c325359079e0123aadbb1203589559dc5cc00e90
Aly <d-aly@outlook.fr>
2024-04-06 19:55:15 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `@import "@/assets/css/app.scss";`,
-      },
-    },
-  },
-});
+module.exports = {
+    chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    }
+}
